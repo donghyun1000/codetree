e7117c1b1052bd8b478c7662dbaa377b305becf0
donghyun <cjs11303@gmail.com>
2026-09-17 14:14:52 +0900

    [Passed] 특이한 출력 (63ms, 16MB)

---
+++ trail1/특이한 출력/special-print.py
@@ -0,0 +1,8 @@
+a = int(input())
+
+for i in range(1,a+1):
+    for j in range(1,a+1):
+        if (i+j)%4 ==0:
+            print(f'({i}, {j})', end='\n')
+        else:
+            print(f'({i}, {j})',end=' ')
