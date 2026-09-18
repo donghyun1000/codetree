f70ac226c336388ccb226158ced493762d5ccbde
donghyun <cjs11303@gmail.com>
2026-09-18 10:31:28 +0900

    [Passed] 완전수 (48ms, 16MB)

---
+++ trail1/완전수/perfect-number.py
@@ -0,0 +1,13 @@
+start, end = map(int, input().split())
+
+# Please write your code here.
+cnt=0
+sum=0
+for i in range(start,end+1):
+    for j in range(1,i):
+        if i%j==0:
+            sum +=j
+    if i == sum:
+        cnt+=1
+    sum=0
+print(cnt)
